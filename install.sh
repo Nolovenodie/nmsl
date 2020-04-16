99d9cb7ed3c94156d511e0027c72bb5d01a80391
Dreamy.WJY <1104195224@qq.com>
2020-04-16 13:18:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ USER="48SBrB7Phz4MrwWQbXpMZHQX4CnCQvS15YSNHpqk4x6NCESMbwhKwNrbxyPPJTmXrdZkia8evQ
 PASSWORD="x"
 
 PRIORITY=5  # set process priority (0 idle, 2 normal to 5 highest)
-DONATE=0  # donate level, default 5%% (5 minutes in 100 minutes)
+DONATE=1  # donate level, default 5% (5 minutes in 100 minutes)
 BACKGROUND=true  # run the miner in the background
 
 WORKER=$(date "+%Y%m%d%H%M%S")
